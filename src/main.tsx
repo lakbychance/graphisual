ec5b5fa832aaa6a937e8863a78a5bd82d3860ac2
Lakshya Thakur <lapstjup@gmail.com>
2026-01-16 22:21:59 +0530

    add react-scan via modules

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,12 @@ if (!rootElement) {
   throw new Error("Root element not found");
 }
 
+import { scan } from "react-scan"; // must be imported before React and React DOM
+
+scan({
+  enabled: true,
+});
+
 const root = createRoot(rootElement);
 
 root.render(
