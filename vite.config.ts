f075f92b9c04301cb1293d784dc56c02aa9c6129
Lakshya Thakur <lapstjup@gmail.com>
2026-01-19 01:16:58 +0530

    more refactor

--- vite.config.ts
+++ vite.config.ts
@@ -37,10 +37,13 @@ export default defineConfig({
       output: {
         manualChunks: {
           "vendor-ui": [
+            "@radix-ui/react-checkbox",
+            "@radix-ui/react-dropdown-menu",
             "@radix-ui/react-popover",
-            "@radix-ui/react-tooltip",
             "@radix-ui/react-slider",
             "@radix-ui/react-tabs",
+            "@radix-ui/react-toggle-group",
+            "@radix-ui/react-tooltip",
           ],
           "vendor-motion": ["motion"],
         },
